ff22b51dbdcc436cb72079503cb1992b081bde53
Kalpesh.Shivannavar <kalpesh89042gmail.com>
2025-02-13 16:34:49 +0530

    Second Commit

--- app/src/main/java/com/example/todo_list/MainActivity.kt
+++ app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.todo_list
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var todoAdapter: TodoAdapter
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
